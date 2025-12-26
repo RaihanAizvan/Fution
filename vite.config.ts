ea2e0453894ebc453e4ee8bf8f0020f0a03fb59a
Mohammed Raihan <aizvanr@gmail.com>
2025-12-26 14:54:16 +0530

    fix: remove deprecated baseUrl and enhance vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,33 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    })
+  ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
   },
   server: {
     port: 3000
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router']
+        }
+      }
+    }
   }
 })
